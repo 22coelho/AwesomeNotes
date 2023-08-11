fd6b428461c26135ecfc1de0684520e9ddbb9035
Tiago Coelho <tiagocoelho@MacBook-Air-de-Tiago.local>
2023-08-11 10:15:15 +0100

    first commit

---
+++ AwesomeNotes/AwesomeNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeNotesApp.swift
+//  AwesomeNotes
+//
+//  Created by Tiago Coelho on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
