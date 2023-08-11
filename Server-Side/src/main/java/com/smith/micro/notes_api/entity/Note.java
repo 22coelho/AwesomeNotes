cecca07e9a0cb1ed95f2190ba896d3c90aac0bac
31coelho <tiagomcoelho22@gmail.com>
2023-08-11 17:50:16 +0100

    server side

---
+++ Server-Side/src/main/java/com/smith/micro/notes_api/entity/Note.java
@@ -0,0 +1,79 @@
+package com.smith.micro.notes_api.entity;
+import org.hibernate.annotations.CreationTimestamp;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name="note")
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    public Note() {
+        // Default constructor
+    }
+
+    public Note(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
